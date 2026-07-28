34c1a6a25d4ef81d741fbe07c5fd56bea986cc3e
sofiapaangel <sofiapaangel@gmail.com>
2026-07-28 11:51:07 -0500

    Pachinko y introduccion a c#

---
+++ MyUnity/Assets/_Main/scprits/Player.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("start");
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {Debug.Log("update");
+    }
+}
