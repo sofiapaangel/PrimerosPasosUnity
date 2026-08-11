8d1f857b79bb28f0aebd9f4d81a3c89ec6a7a75c
sofiapaangel <sofiapaangel@gmail.com>
2026-08-11 12:02:27 -0500

    GroundCheck

---
+++ MyUnity/Assets/_Main/scprits/GroundCheck.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class GroundCheck : MonoBehaviour
+{ public bool isGround;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+       Debug.Log ("Toco el suelo");
+       isGround = true;
+    }
+   
+     private void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log("Estoy saltando");
+        isGround = false;
+        
+    }
+    
+}
+
--- MyUnity/Assets/_Main/scprits/PlayerMovement.cs
+++ MyUnity/Assets/_Main/scprits/PlayerMovement.cs
@@ -3,27 +3,31 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
-    //variables
+        //variables
 
-[SerializeField] private float _force = 10f;
-[SerializeField] private float _speed = 5f;
-[SerializeField] private Rigidbody2D _rigibody2D;
+    [SerializeField] private float _force = 10f;
+    [SerializeField] private float _speed = 5f;
+    [SerializeField] private Rigidbody2D _rigibody2D;
 
-private void Start()
-{
-   _rigibody2D = GetComponent<Rigidbody2D>();
-}
-private void Update()
-{
-    if (Input.GetKey(KeyCode.Space))
+    [SerializeField] private GroundCheck _groundCheck;
+    private void Awake()
+    {
+        _rigibody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (_groundCheck.isGround)
         {
-            _rigibody2D.AddForce(Vector2.up *_force);
-    
-        }
-        _rigibody2D.velocity = Vector2.right * _speed * Time.deltaTime;
-        {
-            
-        }
-}
+            if (Input.GetKey(KeyCode.Space))
+            {
+                _rigibody2D.AddForce(Vector2.up * _force);
+                Debug.Log("Oprimi la tecla");
 
-} 
+            } 
+        }
+
+        _rigibody2D.linearVelocity = Vector2.right * _speed;
+
+    }
+}
