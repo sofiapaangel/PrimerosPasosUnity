5872e4a43958151f01915a14f328e477838c7ed7
Loren0620 <lorennicholson9@gmail.com>
2026-09-01 11:50:51 -0500

    barra de vida

--- MyUnity/Assets/_Main/scprits/Plataformer/TrampaMortal.cs
+++ MyUnity/Assets/_Main/scprits/Plataformer/TrampaMortal.cs
@@ -2,18 +2,17 @@ using UnityEngine;
 
 public class TrampaMortal : MonoBehaviour
 {
+    [SerializeField] private PlayerStats _playerStats1;
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            Debug.Log("El jugador ha muerto");
-            Destroy(collision.gameObject);
+            _playerStats1.RestarVida();
         }
     }
-    
+
 
 
 
 
 }
-    
---
+++ MyUnity/Assets/_Main/scprits/UI/UI Manger.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiManager : MonoBehaviour
+{
+    [SerializeField] private Image barra;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        barra.color = Color.cyan;
+        barra.fillAmount = 0.5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
