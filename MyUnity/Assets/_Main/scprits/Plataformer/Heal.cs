db89a6767364aa41d2d46529ac7c8c9d60af59ba
Loren0620 <lorennicholson9@gmail.com>
2026-09-10 11:57:11 -0500

    Barrita de colores

--- MyUnity/Assets/_Main/scprits/Plataformer/Heal.cs
+++ MyUnity/Assets/_Main/scprits/Plataformer/Heal.cs
@@ -1,17 +1,20 @@
+using TMPro.EditorUtilities;
 using UnityEngine;
 
 public class Heal : MonoBehaviour
 {
     [SerializeField] private PlayerStats _playerStats;
+    [SerializeField] private UIManager _uiManger;
 
     private void OnCollisiononEnter2D(Collision2D collision2D)
     {
-      if (collision2D.gameObject.tag == "Player")
+      if (collision2D.gameObject.tag == ("Player"))
         {
-            Debug.Log("El jugaor ha recibido vida");
+            
             _playerStats.RestaurarVida(10);
+            _uiManger.SumarFillAmount(0.1f);
 
-            Destroy(gameObject);
+            Destroy(this.gameObject);
         }
 
 
--- MyUnity/Assets/_Main/scprits/UI/UI Manger.cs
+++ MyUnity/Assets/_Main/scprits/UI/UI Manger.cs
@@ -1,19 +1,20 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class UiManager : MonoBehaviour
+public class UIManager : MonoBehaviour
 {
-    [SerializeField] private Image barra;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private Image _barra;
+
+    public void SumarFillAmount(float amount)
     {
-        barra.color = Color.cyan;
-        barra.fillAmount = 0.5f;
+        _barra.fillAmount += amount;
     }
-
-    // Update is called once per frame
-    void Update()
+    public void RestarFillAmount(float amount)
     {
-
+        _barra.fillAmount -= amount;
+    }
+    public void ColorBarra(Color color)
+    {
+        _barra.color = color;
     }
 }
