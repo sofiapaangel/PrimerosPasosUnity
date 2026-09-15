b919e071c6d1944708ac7e7a4ed5ef5b22b74654
Loren0620 <lorennicholson9@gmail.com>
2026-09-15 12:01:14 -0500

    la barrita

--- MyUnity/Assets/_Main/scprits/Plataformer/TrampaMortal.cs
+++ MyUnity/Assets/_Main/scprits/Plataformer/TrampaMortal.cs
@@ -1,13 +1,16 @@
+using TMPro.EditorUtilities;
 using UnityEngine;
 
 public class TrampaMortal : MonoBehaviour
 {
     [SerializeField] private PlayerStats _playerStats;
+    [SerializeField] private UIManager _uIManager;
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
             _playerStats.RestarVida(10);
+            _uIManager.RestarFillAmount(0.1f);
         }
     }
 
